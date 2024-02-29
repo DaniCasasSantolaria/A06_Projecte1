deca3ec9bdac4b0b355043dd24148b1a8952f20c
Daniel Casas <daniel.casas@enti.cat>
2024-02-29 14:18:37 +0100

    Salto desde pared (en proceso)

--- Classe/Assets/Scripts/Jump.cs
+++ Classe/Assets/Scripts/Jump.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     Rigidbody2D rb;
+    private float movementSpeed = 3.5f;
 
     [Header ("Jump System")]
     private float jumpTime = 0.35f;
@@ -19,6 +20,16 @@ public class Jump : MonoBehaviour
     bool isJumping;
     float jumpCounter;
 
+    [Header ("Wall Jump System")]
+    public Transform wallCheck;
+    bool isWallTouch;
+    bool isSliding;
+    private float wallSlidingSpeed = 1.5f;
+    private float h;
+    public float wallJumpDuration;
+    public Vector2 wallJumpForce;
+    bool wallJumping;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +40,17 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        h = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             isJumping = true;
             jumpCounter = 0;
+        }
+        else if (isSliding)
+        {
+            wallJumping = true;
+            Invoke("StopWallJump", wallJumpDuration);
         }
 
         if(rb.velocity.y > 0 && isJumping)
@@ -65,7 +82,40 @@ public class Jump : MonoBehaviour
         {
             rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
         }
+        isWallTouch = Physics2D.OverlapBox(wallCheck.position, new Vector2(0.07f, 0.54f), 0, groundLayer);
+
+        if(isWallTouch && !isGrounded() && h != 0)
+        {
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if(isSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        if (wallJumping)
+        {
+            rb.velocity = new Vector2(-h * wallJumpForce.x, wallJumpForce.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(h * movementSpeed, rb.velocity.y);
+        }
+    }
+
+    void StopWallJump()
+    {
+        wallJumping = false;
+    }
+
     bool isGrounded()
     {
         return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.75f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
